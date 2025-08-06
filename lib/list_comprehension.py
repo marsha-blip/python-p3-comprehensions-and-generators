dc5f211f02df9a572e4664ceb19d661a3afb9746
Mary <marshareeitumo@gmail.com>
2025-08-06 14:54:52 +0300

    adds lib

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    """
+    Return a list of all even integers from the input list,
+    using a list comprehension to filter by num % 2 == 0.
+    """
+    return [num for num in num_list if num % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    """
+    Return a new list of sentences where each string has an exclamation
+    mark added at the end, using a list comprehension to transform each item.
+    """
+    return [sentence + "!" for sentence in sentence_list]
